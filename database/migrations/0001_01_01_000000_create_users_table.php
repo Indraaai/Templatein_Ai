92e8c2e1e063777c083af3a8c404a280bfacddfc
Indraaai <iindra09323@gmail.com>
2025-10-14 13:33:49 +0700

    create database and migration

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,6 +17,9 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('role')->default('mahasiswa'); // 'mahasiswa' atau 'admin'
+            $table->foreignId('faculty_id')->nullable()->constrained()->onDelete('set null');
+            $table->foreignId('program_study_id')->nullable()->constrained()->onDelete('set null');
             $table->rememberToken();
             $table->timestamps();
         });
---
+++ database/migrations/2025_10_14_062947_create_templates_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('templates', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->foreignId('faculty_id')->nullable()->constrained()->onDelete('cascade');
+            $table->foreignId('program_study_id')->nullable()->constrained()->onDelete('cascade');
+            $table->json('rules'); // Menyimpan aturan dalam format JSON
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('templates');
+    }
+};
